7f9611dcfc9554b1dc3ce43ef968d29d23e1aed3
M0R <chinmaytlc@gmail.com>
2019-03-06 22:06:19 -0700

    Did Stuff

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,28 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+//import net.fabricmc.example.biome.OceanBigWarmBiome;
+//import net.fabricmc.fabric.impl.registry.RegistryListener;
+import net.minecraft.world.biome.OceanWarmBiome;
 
 public class ExampleMod implements ModInitializer {
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		
+		//OceanBigWarmBiome extremeWarmOcean = null;
+		
 
 		System.out.println("Hello Fabric world!");
+		
+		//Registry.register(extremeWarmOcean, "extremewarmocean", Biome.Category.OCEAN);
+		//Registry.register(Registry.BIOME, "drowned:extremewarmocean", extremeWarmOcean);
+		//Registry.register(registry_1, "extremewarmocean", object_1)
+		
 	}
 }
